141a058cb4e0cbe728aad2bbca3710e91ad7dbb4
Anubhav Tomar <anubhav.tomar94@gmail.com>
2019-03-18 19:49:05 -0400

    js file fix

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$( document ).ready(function() {
+    ga('send', 'pageview', new Date()) ;
+});
+
+$('#resume').click(function() {
+	ga('send', 'event', 'header', 'click', 'resume') ;
+})
+
+$('#linked-in').click(function() {
+	ga('send', 'event', 'header', 'click', 'linked-in') ;
+})
+
+$('#github').click(function() {
+	ga('send', 'event', 'header', 'click', 'github') ;
+})
+
+$('#website').click(function() {
+	ga('send', 'event', 'header', 'click', 'website') ;
+})
+
+$('#header-name').click(function() {
+	ga('send', 'event', 'header', 'click', 'anubhav tomar') ;
+})
+
+$('#email').click(function() {
+	ga('send', 'event', 'header', 'click', 'email') ;
+})
+
+$('#tel-phone').click(function() {
+	ga('send', 'event', 'header', 'click', 'tel-phone') ;
+})
+
+$('#stony').click(function() {
+	ga('send', 'event', 'experience', 'click', 'stony') ;
+})
+
+$('#homebell').click(function() {
+	ga('send', 'event', 'experience', 'click', 'homebell') ;
+})
+
+$('#oyo').click(function() {
+	ga('send', 'event', 'experience', 'click', 'oyo') ;
+})
+
+$('#travelclinic').click(function() {
+	ga('send', 'event', 'experience', 'click', 'travelclinic') ;
+})
+
+$('#drdo').click(function() {
+	ga('send', 'event', 'experience', 'click', 'drdo') ;
+})
